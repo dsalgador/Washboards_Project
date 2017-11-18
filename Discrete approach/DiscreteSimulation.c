103212e5be4027def53639cace27e985c15bc078
Daniel Salgado Rojo <daniel.salgado.rojo@gmail.com>
2017-11-18 18:35:17 +0100

    Add initalisation of ROAD, and defined some structures in the .h file

--- Discrete approach/DiscreteSimulation.c
+++ Discrete approach/DiscreteSimulation.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+//#include <stdbool.h>
 
+#include "DiscreteSimulation.h"
 
 #define F_filename "F_matrix.txt"
+#define f_max (unsigned short) 11
+#define r (unsigned int) 10 //period of the road
+
 
 void WriteMatrixToFile(FILE ** file, char * F){
 	*file = fopen(F_filename, "a");
 	if (*file == NULL){printf("Error changing to append 'a' file!\n");exit(1);	}
 	fprintf(*file, "Some text: %s\n", F);   
     fclose(*file);
-
 }
 
 int main(){
@@ -19,6 +23,33 @@ int main(){
 	//Create, initialise file
 	FILE *f = fopen(F_filename, "w");
 	if (f == NULL){printf("Error opening file!\n");exit(1);	}
+
+	//Initialise the ROAD
+	ROAD F;
+	F.length = r;
+	printf("%d", F.length);
+	F.piles = (PILE *) malloc( ((int) F.length)*sizeof(PILE) );
+	for(int i = 0; i<r;i++){
+		//F.piles[i] = malloc(sizeof(PILE));
+        F.piles[i].height = f_max;
+        //printf("%d", F.piles[i].length);
+        F.piles[i].blocks = (BLOCK *) malloc( ((int) F.piles[i].height)*sizeof(BLOCK));
+        for(int j = 0; j<3;j++){
+        	F.piles[i].blocks[j].filled = 1;
+        	F.piles[i].blocks[j].simbol = 'X';
+
+        }
+        for(int k = 3; k < (int) F.piles[i].height;k++){
+        	F.piles[i].blocks[k].filled = 0;
+        	F.piles[i].blocks[k].simbol = 'O';
+        }
+    }
+
+    //Print ROAD
+    
+
+
+
 
 	/* print some text to the file*/
 
---
+++ Discrete approach/DiscreteSimulation.h
@@ -0,0 +1,18 @@
+
+typedef struct {
+	_Bool filled; // 1 if this block if filled with sand and 0 if not 
+	 char simbol; //"X" or " " if its  filled or not, 
+} BLOCK;
+
+
+typedef struct {
+	unsigned short height; // How many blocks has the pile 'f'
+	BLOCK * blocks; //BLOCKs of the pile, 
+} PILE;
+
+
+typedef struct {
+	unsigned int length; // road length 'r'	
+	PILE * piles; //pointer to the columns (PILEs)
+} ROAD;
+
